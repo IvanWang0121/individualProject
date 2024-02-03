e3fbeaf0cad525c617b033eaad19968f22aea6c2
ivan0121 <ivan0@IVANSLAPTOP>
2024-02-03 13:41:27 +0000

    Initial commit

--- soundfile/shapecheck.py
+++ soundfile/shapecheck.py
@@ -1,7 +1,7 @@
 import librosa
 import numpy as np
 
-data = np.load('D:\individualProject\soundfile\spectral_centroids\converted_female_spectral_centroids.npy',allow_pickle= True)
+data = np.load('D:\\individualProject\\soundfile\\Chroma\\newcombined_chroma_array_female.npy',allow_pickle= True)
 
 print(data.shape)
 
