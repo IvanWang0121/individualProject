cd1d7a6363bc7cd9256b23777d38e2d4c81f619d
ivan0121 <ivan0@IVANSLAPTOP>
2024-02-18 01:00:16 +0000

    Initial commit

--- soundfile/common_method/main.py
+++ soundfile/common_method/main.py
@@ -1,7 +1,7 @@
 import librosa
 import numpy as np
 import os
-
+import soundfile as sf
 
 def load_audio_and_calculate_pitch_mean(audio_path):
 
@@ -19,7 +19,7 @@ def adjust_audio_pitch(audio_path, sr, target_pitch_mean):
 
 
     n_steps = 12 * np.log2(target_pitch_mean / current_pitch_mean)
-    y_adjusted = librosa.effects.pitch_shift(y, sr, n_steps)
+    y_adjusted = librosa.effects.pitch_shift(y, sr=sr, n_steps=n_steps)
     return y_adjusted
 
 
@@ -27,12 +27,17 @@ def adjust_audio_pitch(audio_path, sr, target_pitch_mean):
 male_folder_path = 'D:\individualProject\soundfile\male'
 pitch_means = []
 for filename in os.listdir(male_folder_path):
-    male_pitch_mean, sr = load_audio_and_calculate_pitch_mean(male_folder_path)
+    file_path = os.path.join(male_folder_path, filename)
+    male_pitch_mean, sr = load_audio_and_calculate_pitch_mean(file_path)
+    pitch_means.append(male_pitch_mean)
+print(len(pitch_means))
+means = np.array(pitch_means)
+np.save('meansdata.npy', means)
+maletarget_pitch_mean = np.mean(means)
 
 
-female_voice_path = 'female_voice.mp3'
-y_female_adjusted = adjust_audio_pitch(female_voice_path, sr, male_pitch_mean)
-
+female_voice_path = 'D:\\individualProject\\soundfile\\female\\common_voice_en_38024629.mp3'
+y_female_adjusted = adjust_audio_pitch(female_voice_path, sr, maletarget_pitch_mean)
 
 output_path = 'adjusted_female_voice.wav'
-librosa.output.write_wav(output_path, y_female_adjusted, sr)
+sf.write(output_path, y_female_adjusted, sr)
